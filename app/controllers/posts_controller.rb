85bbc083e1e69c4ea3d615cf39ef61b249ce8bff
Savannah C <sgcorrell@gmail.com>
2019-07-31 19:36:01 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
 
 	def update
 		post = Post.find(params[:id])
-		post.update(params.require(:post).permit(:title, :description, :post_status))
+		post.update(params.require(:post).permit(:title, :description))
 		redirect_to post_path(post)
 	end
 end
